594fc8a27063de6ee8ecb2d808890b261572fb23
Mark Anthony Ponce <markponce07@gmail.com>
2023-03-02 00:12:54 +0800

    run php artisan make:model Make --all

---
+++ app/Http/Controllers/MakeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMakeRequest;
+use App\Http\Requests\UpdateMakeRequest;
+use App\Models\Make;
+
+class MakeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMakeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Make $make)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Make $make)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMakeRequest $request, Make $make)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Make $make)
+    {
+        //
+    }
+}
