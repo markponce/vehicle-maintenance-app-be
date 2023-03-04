73f688215c40bea7b4076b856d71d55f274e46c1
Mark Anthony Ponce <markponce07@gmail.com>
2023-03-05 00:59:09 +0800

    Add token login, and Make CRUD

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\MakeController;
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +19,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return new UserResource($request->user());
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+    
+    return $user->createToken($request->device_name)->plainTextToken;
 });
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return new UserResource($request->user());
+        // return new UserResource(User::with(['makes'])->where('id', $request->user()->id)->first());
+    });
+
+    Route::prefix('make')->group(function () {
+        Route::get('/', [MakeController::class, 'index']);
+        Route::get('/{make}', [MakeController::class, 'show']);
+        Route::put('/{make}', [MakeController::class, 'update']);
+        Route::post('/', [MakeController::class, 'store']);
+        Route::delete('/{make}', [MakeController::class, 'destroy']);
+    });
+});
+
