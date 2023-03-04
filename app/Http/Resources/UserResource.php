bc6c90aa595975e261e4d2f00357c71220617a6e
Mark Anthony Ponce <markponce07@gmail.com>
2023-03-05 00:57:41 +0800

    App\Http\Resources\UserResource include MakeCollection

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -21,6 +21,7 @@ class UserResource extends JsonResource
             'email' => $this->email,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            'makes' => new MakeCollection($this->whenLoaded('makes')),
         ];
     }
 }
